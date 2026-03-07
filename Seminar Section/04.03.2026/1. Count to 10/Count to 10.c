27d471289812798ba563e5f49e525a1eafd948ad
Stefan Kasabov <114361464+stef-kasabov@users.noreply.github.com>
2026-03-07 13:37:17 +0200

    Add files via upload

---
+++ Seminar Section/04.03.2026/1. Count to 10/Count to 10.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+1.	Напишете програма, която отпечатва числата от 1 до 10 като текст;
+*/
+
+int main()
+{
+    for (int i = 1; i <= 10; i++) {
+        printf("%d ", i);
+    }
+    printf("\n");
+    return 0;
+}
